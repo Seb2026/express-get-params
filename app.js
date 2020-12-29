14496b2cefdd59f06c5b5be90da2d8704deed3bf
Sebastian Montoya <seb246@gmail.com>
2020-12-28 21:49:54 -0500

    adding index file

--- app.js
+++ app.js
@@ -5,12 +5,25 @@ const hbs = require(`hbs`);
 app.set(`views`, __dirname + `/views`);
 app.set(`view engine`, `hbs`);
 
-app.get(`/`, (req, res) => {
-    console.log(req);
+app.get(`/`, (req, res, next) => {
+    res.render(`index`);
 });
 
 app.get(`/users/:username`, (req, res) => {
     res.send(req.params);
+});
+
+
+app.get(`/books/:bookid`, (req, res, next) => {
+    res.send(req.params);
+});
+
+app.get(`/users/:username/books/:booksId`, (req, res, next) => {
+    res.send(req.params);
+});
+
+app.get(`/search`, (req, res, next) => {
+    res.send(req.query);
 });
 
 app.listen(3000, () => console.log(`App listening om port 3000!`));
